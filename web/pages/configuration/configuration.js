977ee033543d2fd929a7d4409a717d23ccf288bb
kevin-johnston <kevin@prezoserver.org>
2016-01-30 16:15:57 -0800

    Switched to AngularJS

--- web/pages/configuration/configuration.js
+++ web/pages/configuration/configuration.js
@@ -1,182 +1,129 @@
-Namespace("org.prezoserver.page");
+Namespace("org.prezoserver");
 
-$( document ).ready(function() {
-	var page = new org.prezoserver.page.Configuration();
-	page.init();
-});
-
-org.prezoserver.page.Configuration = function() {
-	var self = this;
+org.prezoserver.ConfigurationController = function($scope) {
+	this.ssi = {checked:false, passed:false, message:null};
+	this.baseHtml = {checked:false, passed:false, message:null};
+	this.prezoJS = {checked:false, passed:false, message:null};
+	this.csrfJS = {checked:false, passed:false, message:null};
 	
-	// tracks whether or not all tests passed
-	var passed = true;
-	
-	/**
-	 * Initializes the page and bind the click event to the 'Run tests' button
-	 */
-	this.init = function() {
-		self.resetTestResults();
-		
-		$("#run_button").bind("click", function() {
-			self.resetTestResults();
-			
-			// contrive some time delays for effect and to allow each test to finish
-			setTimeout(self.testSSI, 500);
-			setTimeout(self.testBase, 1250);
-			setTimeout(self.testPrezoJS, 2000);
-			setTimeout(self.testCsrfJS, 2750);
-			setTimeout(self.report, 3500);
-		});
+	this.reset = function() {
+		this.ssi = {checked:false, passed:false, message:null};
+		this.baseHtml = {checked:false, passed:false, message:null};
+		this.prezoJS = {checked:false, passed:false, message:null};
+		this.csrfJS = {checked:false, passed:false, message:null};
 	};
 	
-	/** 
-	 * Resets the test results.
-	 */
-	this.resetTestResults = function() {
-		passed = true;
+	this.runTests = function() {	
+		setTimeout(this.testSSI, 750);
+		setTimeout(this.testBaseHtml, 1000);
+		setTimeout(this.testPrezoJS, 1250);
+		setTimeout(this.testCsrfJS, 1500);
+		setTimeout(function() {
+			this.testsExecuted = true;
 			
-		// reset ssi check
-		$("#ssi_check_icon").removeClass("icon-check");
-		$("#ssi_check").removeClass("error");
-		$("#ssi_check").removeClass("success");
-		$("#ssi_check_icon").addClass("icon-check-empty");
-		$("#ssi_check").addClass("warning");
-		$("#ssi_check_message").text("Server Side Includes (SSI)");
-		
-		// reset base.html check
-		$("#base_html_icon").removeClass("icon-check");
-		$("#base_html_check").removeClass("error");
-		$("#base_html_check").removeClass("success");
-		$("#base_html_icon").addClass("icon-check-empty");
-		$("#base_html_check").addClass("warning");
-		$("#base_html_message").text("/prezo/fragments/base.html");
-		
-		// reset prezo.js check
-		$("#constants_js_icon").removeClass("icon-check");
-		$("#constants_js_check").removeClass("error");
-		$("#constants_js_check").removeClass("success");
-		$("#constants_js_icon").addClass("icon-check-empty");
-		$("#constants_js_check").addClass("warning");
-		$("#constants_js_message").text("/prezo/js/prezo.js");
-		
-		// reset csrf check
-		$("#csrf_js_icon").removeClass("icon-check");
-		$("#csrf_js_check").removeClass("error");
-		$("#csrf_js_check").removeClass("success");	
-		$("#csrf_js_icon").addClass("icon-check-empty");
-		$("#csrf_js_check").addClass("warning");
-		$("#csrf_js_message").text("/prezo/js/csrf.js");
+			if (this.ssi.passed && this.baseHtml.passed && this.prezoJS.passed && this.csrfJS.passed) {
+				alert("Congratulations! The server configuration appears to be PrezoServer compliant.");
+			} else {
+				alert("Sorry! The server configuration does not appear to be PrezoServer compliant. Check configuration and try again.");
+			}
+		}.bind(this), 1750);
 	};
 	
 	/**
-	 * Checks if SSI is working by trying to find an element that should have
-	 * been included via SSI.
+	 * Checks if SSI is working by finding an element that would only
+	 * be present if the SSI directive worked. In this case the navbar class
+	 * which is only found in the header.
 	 */
 	this.testSSI = function() {
+		this.ssi.checked = true;
+				
 		if ($(".navbar")[0]) {	
-			$("#ssi_check_icon").removeClass("icon-check-empty");
-			$("#ssi_check").removeClass("warning");			
-			$("#ssi_check_icon").addClass("icon-check");
-			$("#ssi_check").addClass("success");			
-			$("#ssi_check_message").append(" ... passed.");
+			this.ssi.passed = true;
+			this.ssi.message = " ... passed.";
 		} else {
-			$("#ssi_check_icon").removeClass("icon-check-empty");
-			$("#ssi_check").removeClass("warning");						
-			$("#ssi_check_icon").addClass("icon-check");
-			$("#ssi_check").addClass("error");
-			$("#ssi_check_message").append(" ... failed.");
-			passed = false;
+			this.ssi.passed = false;
+			this.ssi.message = " ... failed.";
 		}
-	}
+		
+		$scope.$apply();
+	}.bind(this);
 	
 	/**
 	 * Makes an AJAX call for prezo/fragments/base.html. A response status
 	 * of 200 is used to assume the file meets the specification.
 	 */
-	this.testBase = function() {
+	this.testBaseHtml = function() {
+		this.baseHtml.checked = true; 
+		
 		$.ajax({
 			url: "prezo/fragments/base.html",
 			statusCode: {
 				200: function() {
-					$("#base_html_icon").removeClass("icon-check-empty");
-					$("#base_html_check").removeClass("warning");			
-					$("#base_html_icon").addClass("icon-check");
-					$("#base_html_check").addClass("success");			
-					$("#base_html_message").append(" ... passed.");
-				}
+					this.baseHtml.passed = true;
+					this.baseHtml.message = " ... passed.";
+					$scope.$apply();
+				}.bind(this)
 			},
 			error: function() {
-				$("#base_html_icon").removeClass("icon-check-empty");
-				$("#base_html_check").removeClass("warning");						
-				$("#base_html_icon").addClass("icon-check");
-				$("#base_html_check").addClass("error");
-				$("#base_html_message").append(" ... failed.");
-				passed = false;
-			}
+				this.baseHtml.passed = false;
+				this.baseHtml.message = " ... failed.";
+				$scope.$apply();
+			}.bind(this)
 		});
-	}
+	}.bind(this);
 	
 	/**
 	 * Tests that the path prezo/js/prezo.js exists.
 	 */
 	this.testPrezoJS = function() {
+		this.prezoJS.checked = true;
+		
 		$.ajax({
 			url: "prezo/js/prezo.js",
 			statusCode: {
 				200: function() {
-					$("#constants_js_icon").removeClass("icon-check-empty");
-					$("#constants_js_check").removeClass("warning");			
-					$("#constants_js_icon").addClass("icon-check");
-					$("#constants_js_check").addClass("success");			
-					$("#constants_js_message").append(" ... passed.");
-				}
+					this.prezoJS.passed = true;
+					this.prezoJS.message = " ... passed.";
+					$scope.$apply();
+				}.bind(this)
 			},
 			error: function() {
-				$("#constants_js_icon").removeClass("icon-check-empty");
-				$("#constants_js_check").removeClass("warning");						
-				$("#constants_js_icon").addClass("icon-check");
-				$("#constants_js_check").addClass("error");
-				$("#constants_js_message").append(" ... failed.");
-				passed = false;
-			}
+				this.prezoJS.passed = false;
+				this.prezoJS.message = " ... failed.";
+				$scope.$apply();
+			}.bind(this)
 		});
-	};
+	}.bind(this);
 	
 	/**
 	 * Tests that the path prezo/js/csrf.js exists.
 	 */
 	this.testCsrfJS = function() {
+		this.csrfJS.checked = true;
+		
 		$.ajax({
 			url: "prezo/js/csrf.js",
 			dataType: "html",
 			statusCode: {
 				200: function() {
-					$("#csrf_js_icon").removeClass("icon-check-empty");
-					$("#csrf_js_check").removeClass("warning");			
-					$("#csrf_js_icon").addClass("icon-check");
-					$("#csrf_js_check").addClass("success");			
-					$("#csrf_js_message").append(" ... passed.");
-				}
+					this.csrfJS.passed = true;
+					this.csrfJS.message = " ... passed.";
+					$scope.$apply();
+				}.bind(this)
 			},
 			error: function() {
-				$("#csrf_js_icon").removeClass("icon-check-empty");
-				$("#csrf_js_check").removeClass("warning");						
-				$("#csrf_js_icon").addClass("icon-check");
-				$("#csrf_js_check").addClass("error");
-				$("#csrf_js_message").append(" ... failed.");
-				passed = false;
-			}
+				this.csrfJS.passed = false;
+				this.csrfJS.message = " ... failed.";
+				$scope.$apply();
+			}.bind(this)
 		});
-	};
+	}.bind(this);
+};
+
+(function() {
+	var app = angular.module('configuration', []);
 	
-	/**
-	 * Once all tests are complete this provides a report of success or failure.
-	 */
-	this.report = function() {
-		if (passed) {
-			alert("Congratulations! The server configuration appears to be PrezoServer compliant.");
-		} else {
-			alert("Sorry! The server configuration does not appear to be PrezoServer compliant. Check configuration and try again.");
-		}	
-	}
-}
+	app.controller('ConfigurationController', org.prezoserver.ConfigurationController);
+})();
+
+
